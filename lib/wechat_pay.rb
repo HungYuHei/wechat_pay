4368d2403aee2f91493b8b67ede53e4dcece7d9f
HungYuHei <kongruxi@gmail.com>
2014-08-20 12:03:22 +0800

    Clean up code

--- lib/wechat_pay.rb
+++ lib/wechat_pay.rb
@@ -1,18 +1,14 @@
-require 'wechat_pay/version'
-require 'wechat_pay/access_token'
 require 'wechat_pay/app'
 require 'wechat_pay/sign'
 require 'wechat_pay/utils'
 require 'wechat_pay/notify'
-require 'wechat_pay/prepay_id'
+require 'wechat_pay/version'
 require 'wechat_pay/package'
+require 'wechat_pay/prepay_id'
+require 'wechat_pay/access_token'
 
 module WechatPay
   class << self
-    attr_accessor :app_id
-    attr_accessor :app_secret
-    attr_accessor :pay_sign_key
-    attr_accessor :partner_id
-    attr_accessor :partner_key
+    attr_accessor :app_id, :app_secret, :pay_sign_key, :partner_id, :partner_key
   end
 end
