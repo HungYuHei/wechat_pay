9f4060c36636451b9b5a386ac62f92b411086beb
HungYuHei <kongruxi@gmail.com>
2014-09-28 15:57:23 +0800

    Refactor WechatPay::DeliverNotify

--- lib/wechat_pay/deliver_notify.rb
+++ lib/wechat_pay/deliver_notify.rb
@@ -12,17 +12,7 @@ module WechatPay
         deliver_timestamp: Time.now.to_i.to_s
       }.merge(params)
 
-      sign_attrs = {
-        appid:             WechatPay.app_id,
-        appkey:            WechatPay.pay_sign_key,
-        openid:            params[:openid],
-        transid:           params[:transid],
-        deliver_msg:       params[:deliver_msg],
-        out_trade_no:      params[:out_trade_no],
-        deliver_status:    params[:deliver_status],
-        deliver_timestamp: params[:deliver_timestamp]
-      }
-      app_signature = Sign.sha1(sign_attrs)
+      app_signature = generate_app_signature(params)
 
       data = {
         appid:             WechatPay.app_id,
@@ -39,6 +29,17 @@ module WechatPay
       RestClient.post(url, JSON.generate(data)) do |response|
         JSON.parse(response.body, symbolize_names: true)
       end
+    end
+
+    private
+
+    def self.generate_app_signature(params)
+      attrs = {
+        appid: WechatPay.app_id,
+        appkey: WechatPay.pay_sign_key
+      }.merge(params)
+
+      Sign.sha1(attrs)
     end
   end
 end
